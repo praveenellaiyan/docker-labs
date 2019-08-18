c16420b393ecc65c8030e2cf42ddc77f58d24a72
praveenellaiyan <praveen.ellaiyan@gmail.com>
2019-08-18 16:37:32 +0530

    docker execute custom script when container starts

--- example-project/dockerize-springboot-execute-custom-script/src/test/java/com/praveenellayian/dockerize/application/DockerizeSpringbootApplicationTests.java
+++ example-project/dockerize-springboot-execute-custom-script/src/test/java/com/praveenellayian/dockerize/application/DockerizeSpringbootApplicationTests.java
@@ -1,11 +1,9 @@
-package com.praveenellayian.dockerize.springboot;
+package com.praveenellayian.dockerize.application;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.praveenellayian.dockerize.application.DockerizeSpringbootApplicationTests;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = DockerizeSpringbootApplicationTests.class)
